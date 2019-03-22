435a03c9d8e5937a48a3419614562da6fa7c991c
br3fc00l <461761446@qq.com>
2019-03-21 22:55:09 -0400

    android initial commit

---
+++ android/HealthApplication/app/src/main/java/bu/edu/ec500/healthapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package bu.edu.ec500.healthapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import bu.edu.ec500.healthapplication.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ android/HealthApplication/app/src/main/java/bu/edu/ec500/healthapplication/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package bu.edu.ec500.healthapplication.ui.main;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
