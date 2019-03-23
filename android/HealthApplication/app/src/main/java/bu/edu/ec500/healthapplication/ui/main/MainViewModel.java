a0d8ed7bbdf839c5a94a36ee19b51f0eaa24db6f
br3fc00l <461761446@qq.com>
2019-03-22 20:14:19 -0400

    remove dummy application file

--- android/HealthApplication/app/src/main/java/bu/edu/ec500/healthapplication/ui/main/MainViewModel.java
+++
@@ -1,7 +0,0 @@
-package bu.edu.ec500.healthapplication.ui.main;
-
-import android.arch.lifecycle.ViewModel;
-
-public class MainViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
